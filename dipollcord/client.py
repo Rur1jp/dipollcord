bdf0b3086aeb2925d72a45b6945bc88851cbc9be
るる <rurusama.77@gmail.com>
2025-12-06 10:30:01 +0900

    Update client.py

--- dipollcord/client.py
+++ dipollcord/client.py
@@ -1,8 +1,43 @@
+import discord
 import requests
 
 class pollclient:
     def __init__(self, token: str):
         self.token = token
+
+    async def userinstallpoll(
+        self,
+        interaction,
+        content: str,
+        question: str,
+        answers: list[str],
+        duration: int = 1,
+        allow_multiselect: bool = False
+    ):
+        bot = interaction.client
+
+        route = discord.http.Route(
+            "POST",
+            "/webhooks/{bot_id}/{token}",
+            bot_id=bot.user.id,
+            token=interaction.token
+        )
+
+        json = {
+            "content": content,
+            "poll": {
+                "question": {"text": question},
+                "answers": [
+                    {"poll_media": {"text": a}}
+                    for a in answers
+                ],
+                "allow_multiselect": allow_multiselect,
+                "duration": duration,
+                "layout_type": 1
+            }
+        }
+
+        await bot.http.request(route, json=json)
 
     def sendPoll(
         self,
@@ -14,12 +49,13 @@ class pollclient:
         allow_multiselect: bool = False
     ):
         url = f"https://discord.com/api/v10/channels/{channel_id}/messages"
+
         headers = {
             "Authorization": f"Bot {self.token}",
             "Content-Type": "application/json"
         }
 
-        json = {
+       json = {
             "content": content,
             "poll": {
                 "question": {"text": question},
